48397e2b42d386f2d43eee01ded44dc5bf966c4a
R_Ashfell <luqmanulraiserhakim@gmail.com>
2021-10-23 22:48:10 +0800

    Submit button works but it does not reset the input field

--- myScript.js
+++ myScript.js
@@ -4,7 +4,7 @@ function Book() {
   // the constructor...
 }
 
-function addBookToLibrary(title,author,page,status) {
+function addBookToLibrary(title,author,page,readStatus) {
   this.title = title;
   this.author = author;
   this.page = page;
@@ -19,8 +19,27 @@ function closeForm() {
   document.getElementById('modal').style.display = 'none';
 }
 
+function resetForm() {
+  
+}
+
+function addBook() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const page = document.getElementById('page').value;
+  const readStatus = document.getElementById('readStatus').value;
+  console.log(title);
+  console.log(author);
+  console.log(page);
+  console.log(readStatus);
+  closeForm();
+}
+
 const inputPop = document.getElementById('add');
 inputPop.addEventListener('click', openForm)
 
 const inputHide = document.getElementById('cancel');
 inputHide.addEventListener('click', closeForm)
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', addBook);
