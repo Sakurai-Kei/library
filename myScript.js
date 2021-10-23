1435a5b4bddd7a4c8444f43d588cb34e2e0bcc4e
sakurai-kei <luqmanulraiserhakim@gmail.com>
2021-10-24 04:28:01 +0800

    Upon submitting a book, the book will be automatically displayed. However, upon submitting a second book, the list will display the first book again along with the second book, resulting in total three books.

--- myScript.js
+++ myScript.js
@@ -1,14 +1,22 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
-
-function addBookToLibrary(title,author,page,readStatus) {
+function Book(title,author,page,readStatus) {
   this.title = title;
   this.author = author;
   this.page = page;
   this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const page = document.getElementById('page').value;
+  const readStatus = document.getElementById('readStatus').value;
+  const book = new Book(title,author,page,readStatus);
+  myLibrary.push(book);
+  resetForm();
+  closeForm();
+  displayBook();
 }
 
 function openForm() {
@@ -27,24 +35,29 @@ function resetForm() {
 }
 
 function displayBook() {
-  
-}
-
-function addBook() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const page = document.getElementById('page').value;
-  const readStatus = document.getElementById('readStatus').value;
-  const book = new addBookToLibrary(title,author,page,readStatus);
-  myLibrary.push(book);
-  console.table(book);
-  console.log(myLibrary);
-  // console.log(title);
-  // console.log(author);
-  // console.log(page);
-  // console.log(readStatus);
-  resetForm();
-  closeForm();
+  const screen = document.getElementById('bookList');
+  myLibrary.forEach(book => {
+    const div = document.createElement('div');
+    div.classList.add('book');
+    const bookContainer = screen.appendChild(div);
+    const h1 = document.createElement('h1');
+    const h3 = document.createElement('h3');
+    const divPage = document.createElement('div');
+    h1.textContent = book.title;
+    h3.textContent = book.author;
+    divPage.textContent = `${book.page} pages`;
+    switch(book.readStatus){
+      case "Yes":
+        div.classList.add('read');
+        break;
+      case "No":
+        div.classList.add('notRead');
+        break;  
+    }
+    bookContainer.appendChild(h1);
+    bookContainer.appendChild(h3);
+    bookContainer.appendChild(divPage);
+  })
 }
 
 const inputPop = document.getElementById('add');
@@ -54,4 +67,4 @@ const inputHide = document.getElementById('cancel');
 inputHide.addEventListener('click', closeForm)
 
 const submit = document.getElementById('submit');
-submit.addEventListener('click', addBook);
+submit.addEventListener('click', addBookToLibrary);
